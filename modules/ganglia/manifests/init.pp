bb3076812c0c3d4ff2199e56645f36d665501b2b
Uwe Janssen <mail@uwe-janssen.de>
2014-05-10 08:47:07 +0200

    add gmetad config

--- modules/ganglia/manifests/init.pp
+++ modules/ganglia/manifests/init.pp
@@ -6,13 +6,30 @@ class ganglia ($node_type = 'master') {
     }
 
 	package { 'ganglia-monitor':
-		ensure => "installed",
+		ensure => 'installed',
 	}
 
-	file { '/tmp/test-ganglia-gmond.conf':
+	file { '/etc/ganglia/gmond.conf':
         ensure => file,
         source => "puppet:///modules/ganglia/gmond-${node_type}.conf",
         notify => Service['ganglia-monitor'],
+    }
+
+    if $node_type == 'master' {
+ 	service { 'gmetad':
+ 		require => Package['gmetad'],
+        ensure  => running,
+    }
+
+	package { 'gmetad':
+		ensure => 'installed',
+	}
+
+	file { '/etc/ganglia/gmetad.conf':
+        ensure => file,
+        source => "puppet:///modules/ganglia/gmetad.conf",
+        notify => Service['gmetad'],
+    }
     }
 }
 
