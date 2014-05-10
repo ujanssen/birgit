ec3e225db60277e648358e12bc1c3494bca433ed
Uwe Janssen <mail@uwe-janssen.de>
2014-05-10 08:38:13 +0200

    fix double quotes

--- modules/ganglia/manifests/init.pp
+++ modules/ganglia/manifests/init.pp
@@ -1,18 +1,18 @@
 class ganglia ($node_type = 'master') {
 
  	service { 'ganglia-monitor':
- 		require => Package["ganglia-monitor"],
+ 		require => Package['ganglia-monitor'],
         ensure  => running,
     }
 
-	package { "ganglia-monitor":
-		ensure => "installed"
+	package { 'ganglia-monitor':
+		ensure => "installed",
 	}
 
-	file { "/tmp/ganglia-gmond.conf":
+	file { '/tmp/test-ganglia-gmond.conf':
         ensure => file,
         source => "puppet:///modules/ganglia/gmond-${node_type}.conf",
-        notify => Service["ganglia-monitor'"]
+        notify => Service['ganglia-monitor'],
     }
 }
 
