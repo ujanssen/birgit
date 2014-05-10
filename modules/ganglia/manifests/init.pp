4f62c48c334877126daffa27669737e548837a5e
Uwe Janssen <mail@uwe-janssen.de>
2014-05-10 08:35:30 +0200

    ganglia monitor is running

--- modules/ganglia/manifests/init.pp
+++ modules/ganglia/manifests/init.pp
@@ -1,10 +1,18 @@
 class ganglia ($node_type = 'master') {
+
+ 	service { 'ganglia-monitor':
+ 		require => Package["ganglia-monitor"],
+        ensure  => running,
+    }
+
 	package { "ganglia-monitor":
 		ensure => "installed"
 	}
 
-	file { "/tmp/ganglia":
-		content => "$node_type"
-	}
+	file { "/tmp/ganglia-gmond.conf":
+        ensure => file,
+        source => "puppet:///modules/ganglia/gmond-${node_type}.conf"
+        notify => Service["ganglia-monitor'"]
+    }
 }
 
